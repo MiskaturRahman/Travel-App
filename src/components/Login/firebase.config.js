85959b839b5dc971e53988b3639ec18e2c106645
Misk007 <mdrahmanmiskatur@gmail.com>
2020-11-08 17:46:21 +0600

    travel app done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZnsa4dP4BUecyqYSsOynGAzNvKHRcpkQ",
+    authDomain: "travel-guru-by-misk.firebaseapp.com",
+    databaseURL: "https://travel-guru-by-misk.firebaseio.com",
+    projectId: "travel-guru-by-misk",
+    storageBucket: "travel-guru-by-misk.appspot.com",
+    messagingSenderId: "121156922955",
+    appId: "1:121156922955:web:174dfe5f6156a82a7467ce"
+};
+
+export default firebaseConfig;
